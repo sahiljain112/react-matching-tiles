1504e989c32b949e04c24587afdab19042616f51
sahiljain112 <sahiljain112@gmail.com>
2017-02-03 20:26:47 +0530

    Setup

---
+++ README.md
@@ -0,0 +1,4 @@
+### Tiles
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+It is a simple project that simulates a game of matching tiles.
---
+++ src/components/app/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="tile-container">
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+        <div className="tile">
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
